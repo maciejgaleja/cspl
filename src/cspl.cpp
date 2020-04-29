e51443f375c5e10252a06ad677281549b2bd9371
Maciej Galeja <maciej.galeja@outlook.com>
2020-04-29 09:48:18 +0200

    Add base for spell checking

--- src/cspl.cpp
+++ src/cspl.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+#include <hunspell.hxx>
+
+void check_word(Hunspell& hs, const std::string& word) {
+  bool ok = hs.spell(word.c_str());
+  if (ok) {
+    std::cout << word << "\n";
+  } else {
+    auto alternatives = hs.suggest(word);
+    for (const auto& s : alternatives) {
+      std::cout << word << " -> " << s << "\n";
+    }
+  }
+}
+
 int main(int argc, char** argv) {
   std::cout << "cspl\n";
+  Hunspell hs("C:\\Hunspell\\en_US.aff", "C:\\Hunspell\\en_US.dic");
+
+  std::string word = "waitign";
+  check_word(hs, word);
 }

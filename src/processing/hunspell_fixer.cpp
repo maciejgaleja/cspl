85a6c7e023db9df76e904c7bf0423daaee8a316d
Maciej Galeja <maciej.galeja@outlook.com>
2020-05-01 17:04:32 +0200

    Add option to specify custom word as correction

--- src/processing/hunspell_fixer.cpp
+++ src/processing/hunspell_fixer.cpp
@@ -45,6 +45,10 @@ void HunspellFixer::add(const Word& word)
             m_dict.add(word);
             notify_all(word);
         }
+		else if (choice.choice == UserDecision::Choice::CUSTOM)
+		{
+            add(choice.custom_word);
+		}
         else
         {
             notify_all(alternatives[choice.chosen_word]);
@@ -72,6 +76,7 @@ UserDecision HunspellFixer::ask_user(std::string error_word,
     }
     cout << "i: ignore\n";
     cout << "a: add to dictionary\n";
+    cout << "c: custom word\n";
 
     std::string decision_str;
     cin >> decision_str;
@@ -87,6 +92,13 @@ UserDecision HunspellFixer::ask_user(std::string error_word,
             ret.choice = UserDecision::Choice::IGNORE;
         else if(decision_str == "a")
             ret.choice = UserDecision::Choice::ADD;
+        else if(decision_str == "c")
+        {
+            ret.choice = UserDecision::Choice::CUSTOM;
+            cout << "Type the correct wor below\n";
+            cin >> decision_str;
+            ret.custom_word = decision_str;
+        }
         else
         {
             cout << "Invalid option";
